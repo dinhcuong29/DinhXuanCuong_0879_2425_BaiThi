72a9b9dc9257cc0ca929556cea70cd1278733c76
dinhcuong29 <cuonga521dqh@gmail.com>
2025-01-02 14:28:35 +0700

    CREATE_PROJECT

---
+++ DinhXuanCuong_0879_2425_BaiThi/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DinhXuanCuong_0879_2425_BaiThi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
